e1b68be5df2f2eb4888441d4e5d6bb4fb6954708
cjlalala <cj3.huang@metavibe.ai>
2026-04-20 17:58:27 +0800

    fix: update API destination URLs in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -34,23 +34,23 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/user/:path*",
-        destination: "https://main-service-prod.metavibe-api.com/user/:path*",
+        destination: "https://faithtime-mob.metavibe-api.com/user/:path*",
         // destination: "http://localhost:5000/user/:path*",
       },
       {
         source: "/api/notification/:path*",
         destination:
-          "https://main-service-prod.metavibe-api.com/notification/:path*",
+          "https://faithtime-mob.metavibe-api.com/notification/:path*",
         // "http://localhost:5000/notification/:path*",
       },
       {
         source: "/api/auth/:path*",
-        destination: "https://main-service-prod.metavibe-api.com/auth/:path*",
+        destination: "https://faithtime-mob.metavibe-api.com/auth/:path*",
         // destination: "http://localhost:5000/auth/:path*",
       },
       {
         source: "/api/assets/:path*",
-        destination: "https://main-service-prod.metavibe-api.com/assets/:path*",
+        destination: "https://faithtime-mob.metavibe-api.com/assets/:path*",
         // destination: "http://localhost:5000/assets/:path*",
       },
     ];
